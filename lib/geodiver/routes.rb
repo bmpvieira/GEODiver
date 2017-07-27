43ffd4a81ae9b26e7d5ff6b214e5de1ad5093aea
Ismail Moghul <ismail.moghul@gmail.com>
2017-07-27 23:56:29 +0100

    add `verify_iss: false` to omniauth config

--- lib/geodiver/routes.rb
+++ lib/geodiver/routes.rb
@@ -41,7 +41,7 @@ module GeoDiver
       # Provide OmniAuth the Google Key and Secret Key for Authentication
       use OmniAuth::Builder do
         provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'],
-                 provider_ignores_state: true
+                 provider_ignores_state: true, verify_iss: false
       end
 
       # view directory will be found here.
